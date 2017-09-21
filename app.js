d2a6dfe66615ed7689d1beb77e326b19796b48ca
PJC-1 <jumpzine@gmail.com>
2017-09-20 22:28:44 -0700

    installed express and set up the server

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const express = require('express');
 const dotenv = require('dotenv');
 
 // DOTENV
 dotenv.config();
 
 console.log(process.env.YOUR_API_KEY);
+
+// Init app
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+app.listen(3000, function(){
+    console.log('You are now listening to the smooth sounds of port 3000...');
+});
