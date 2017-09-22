b9c63c0e3d9a8e2b527ceaa31671d81ff014a3f9
PJC-1 <jumpzine@gmail.com>
2017-09-21 21:24:00 -0700

    using the sendFile express method to render a plain HTML file rather than use an templating engine

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const dotenv = require('dotenv');
+
 
 // DOTENV
 dotenv.config();
@@ -9,8 +11,32 @@ console.log(process.env.YOUR_API_KEY);
 // Init app
 const app = express();
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+// Load View
+app.set('views', path.join(__dirname, 'views'));
+
+// Set public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', function(req, res, next){
+    var options = {
+      root: __dirname + '/views/',
+      dotfiles: 'deny',
+      headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+      }
+    };
+
+    var fileName = 'index.html'
+    res.sendFile(fileName, options, function(err){
+        if(err) {
+          next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 });
 
 app.listen(3000, function(){
