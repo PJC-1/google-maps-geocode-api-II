6153e6ff373990d0e8d9015c4c3bb7b3f0380139
PJC-1 <jumpzine@gmail.com>
2017-09-23 16:39:03 -0700

    making a get request and outputting the formatted address to the view SUCCESS

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,20 @@ function geocode(){
         }
     })
     .then(function(response){
+        // Log full response
         console.log(response);
+
+        // Format response
+        console.log(response.data.results[0].formatted_address);
+        var formattedAddress = response.data.results[0].formatted_address;
+        var formattedAddressOutput = `
+          <ul class="list-group">
+            <li class="list-group-item">${formattedAddress}</li>
+          </ul>
+        `;
+
+        // Output to app
+        document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
     })
     .catch(function(error){
         console.log(error);
