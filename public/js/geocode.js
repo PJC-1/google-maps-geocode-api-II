0c7c6b42c183d3c2bda402fe85c1cf94ce382eb2
PJC-1 <jumpzine@gmail.com>
2017-10-05 22:01:04 -0700

    adding a marker from the data received from server via geocode api SUCCESS

--- public/js/geocode.js
+++ public/js/geocode.js
@@ -18,10 +18,22 @@ $(document).ready(function(){
       console.log("latitude ", geocode.results[0].geometry.location.lat);
       console.log("longitude ", geocode.results[0].geometry.location.lng);
       console.log("address ", geocode.results[0].formatted_address);
+      lat = geocode.results[0].geometry.location.lat;
+      lng = geocode.results[0].geometry.location.lng;
+      address = geocode.results[0].formatted_address;
+      addMarker2(lat,lng,map);
     }
 
     function handleError(err){
       console.log(err);
+    }
+
+    function addMarker2(latitude,longitude,useMap){
+      var latLng = new google.maps.LatLng(latitude, longitude);
+      new google.maps.Marker({
+          position: latLng,
+          map:useMap
+      });
     }
 });
 
