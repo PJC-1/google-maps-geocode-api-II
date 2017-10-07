f546e8715ac479c0fbc2d280b984598250499bd0
PJC-1 <jumpzine@gmail.com>
2017-10-06 21:35:33 -0700

    changed arrangement of initMap function

--- public/js/main.js
+++ public/js/main.js
@@ -3,68 +3,8 @@ console.log("Sanity Check: JS is working!");
 var locationForm = document.getElementById('location-form');
 // Set map variable definition here to give the Maps object global scope
 var map;
-
-$(document).ready(function(){
-  $.ajax({
-    method: 'GET',
-    url: '/api/locations',
-    success: handleSuccess,
-    error: handleError
-  });
-
-
-
-
-
-
-  // addMarker2
-  function addMarker2(latitude,longitude,useMap){
-    var latLng = new google.maps.LatLng(latitude, longitude);
-    new google.maps.Marker({
-        position: latLng,
-        map:useMap
-    });
-  }
-
-  // Add Marker Function
-  function addMarker(props){
-    var marker = new google.maps.Marker({
-        position: props.coords,
-        map:map
-    });
-    // Check for customicon
-    if(props.iconImage){
-        // Set icon image
-        marker.setIcon(props.iconImage);
-    }
-    // Check for content
-    if(props.content){
-        var infoWindow = new google.maps.InfoWindow({
-            content:props.content
-        });
-
-        marker.addListener('click', function(){
-            infoWindow.open(map, marker);
-        });
-    }
-  }
-
-  function handleSuccess(response){
-    var locations = response;
-    console.log('logging server response: ', locations);
-    for( var i=0 ; i<locations.length ; i++ ){
-      var lat = locations[i].latitude;
-      var lng = locations[i].longitude;
-      addMarker2(lat,lng,map);
-    }
-  }
-
-  function handleError(err){
-    console.log(err);
-  }
-
-});
-
+var lat;
+var lng;
 
 function initMap(){
     // Map options
@@ -76,67 +16,34 @@ function initMap(){
     map = new google.maps.Map(document.getElementById('map'), options);
 }
 
-// //Listen for submit
-// locationForm.addEventListener('submit', geocode);
-//
-// function geocode(e){
-//     // Precent actual submit
-//     e.preventDefault();
-//
-//     var location = document.getElementById('location-input').value;
-//
-//     axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
-//         params:{
-//             address: location,
-//             key:'AIzaSyAD_OJUoh5BPPl1WUGJEl3G9WMj4taITLs'
-//         }
-//     })
-//     .then(function(response){
-//         // Log full response
-//         console.log(response);
-//
-//         // Format response
-//         var formattedAddress = response.data.results[0].formatted_address;
-//         console.log(formattedAddress);
-//         var formattedAddressOutput = `
-//           <ul class="list-group">
-//             <li class="list-group-item">${formattedAddress}</li>
-//           </ul>
-//         `;
-//
-//         // Address Components
-//         var addressComponents = response.data.results[0].address_components;
-//         console.log(addressComponents);
-//         var addressComponentsOutput = `<ul class="list-group">`;
-//         for(var i = 0;i < addressComponents.length;i++){
-//             addressComponentsOutput += `
-//               <li class="list-group-item"><strong>${addressComponents[i].types[0]}</strong>: ${addressComponents[i].long_name}</li>
-//             `
-//         }
-//         addressComponentsOutput += '</ul>';
-//
-//         // Geometry
-//         var lat = response.data.results[0].geometry.location.lat;
-//         var lng = response.data.results[0].geometry.location.lng;
-//         var geometryOutput = `
-//           <ul class="list-group">
-//             <li class="list-group-item"><strong>Latitude</strong>: ${lat}</li>
-//             <li class="list-group-item"><strong>Longitude</strong>: ${lng}</li>
-//           </ul>
-//         `;
-//
-//         // Output to app
-//         document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
-//         document.getElementById('address-components').innerHTML = addressComponentsOutput;
-//         document.getElementById('geometry').innerHTML = geometryOutput;
-//
-//         addMarker2(lat,lng,map);
-//
-//         var testHtml = testTemplate(lat);
-//         $('#test-div').html(testHtml);
-//
-//     })
-//     .catch(function(error){
-//         console.log(error);
-//     });
-// }
+function addMarker(latitude,longitude,useMap){
+  var latLng = new google.maps.LatLng(latitude, longitude);
+  new google.maps.Marker({
+      position: latLng,
+      map:useMap
+  });
+}
+
+$(document).ready(function(){
+  $.ajax({
+    method: 'GET',
+    url: '/api/locations',
+    success: handleSuccess,
+    error: handleError
+  });
+
+  function handleSuccess(response){
+    var locations = response;
+    console.log('logging server response: ', locations);
+    for( var i=0 ; i<locations.length ; i++ ){
+      lat = locations[i].latitude;
+      lng = locations[i].longitude;
+      addMarker(lat,lng,map);
+    }
+  }
+
+  function handleError(err){
+    console.log(err);
+  }
+
+});
