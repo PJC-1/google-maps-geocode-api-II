23b297daf60bc786eed0bc8a84a97724fbad52e1
PJC-1 <jumpzine@gmail.com>
2017-10-06 23:31:55 -0700

    converted to a synchronous loading of the map to avoid conflict with ajax

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,45 @@
 console.log("Sanity Check: JS is working!");
-// Get Location form
-var locationForm = document.getElementById('location-form');
 // Set map variable definition here to give the Maps object global scope
 var map;
 var lat;
 var lng;
+
+$(document).ready(function(){
+  initMap();
+  fetchLocations();
+
+});
+
+function addMarker(latitude,longitude,useMap){
+  var latLng = new google.maps.LatLng(latitude, longitude);
+  new google.maps.Marker({
+      position: latLng,
+      map:useMap
+  });
+}
+
+function fetchLocations(){
+  $.ajax({
+    method: 'GET',
+    url: '/api/locations',
+    success: handleSuccess,
+    error: handleError
+  });
+}
+
+function handleSuccess(response){
+  var locations = response;
+  console.log('logging server response: ', locations);
+  for( var i=0 ; i<locations.length ; i++ ){
+    lat = locations[i].latitude;
+    lng = locations[i].longitude;
+    addMarker(lat,lng,map);
+  }
+}
+
+function handleError(err){
+  console.log(err);
+}
 
 function initMap(){
     // Map options
@@ -15,35 +50,3 @@ function initMap(){
     // New map
     map = new google.maps.Map(document.getElementById('map'), options);
 }
-
-function addMarker(latitude,longitude,useMap){
-  var latLng = new google.maps.LatLng(latitude, longitude);
-  new google.maps.Marker({
-      position: latLng,
-      map:useMap
-  });
-}
-
-$(document).ready(function(){
-  $.ajax({
-    method: 'GET',
-    url: '/api/locations',
-    success: handleSuccess,
-    error: handleError
-  });
-
-  function handleSuccess(response){
-    var locations = response;
-    console.log('logging server response: ', locations);
-    for( var i=0 ; i<locations.length ; i++ ){
-      lat = locations[i].latitude;
-      lng = locations[i].longitude;
-      addMarker(lat,lng,map);
-    }
-  }
-
-  function handleError(err){
-    console.log(err);
-  }
-
-});
