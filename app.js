0ba5da292f4af70d6e485ea220f65662602e45dd
PJC-1 <jumpzine@gmail.com>
2017-10-12 21:07:39 -0700

    added some notes to build out the post route to handle the formdata

--- app.js
+++ app.js
@@ -112,6 +112,9 @@ app.post('/api/formData', function(req,res){
             res.send('error from req to geocode API', error);
         } else {
             var geocodeOutput = JSON.parse(body);
+            // from here you can create a new location object
+            // the save the new location to the db
+            // then send the saved entry to the client
             res.json(geocodeOutput);
         }
     })
