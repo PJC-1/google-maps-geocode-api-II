5c50e8858c41ff70995b410c74d2959524f2e322
PJC-1 <jumpzine@gmail.com>
2017-10-27 21:35:20 -0700

    using window.location.pathname to access and use url

--- public/js/editLocation.js
+++ public/js/editLocation.js
@@ -1,1 +1,10 @@
 console.log("testing edit location...");
+
+$(document).ready(function(){
+    var windowPath = window.location.pathname;
+    var windowPathSplit = windowPath.split('/');
+    var id = windowPathSplit[2];
+    var url = '/api/locations/' + id;
+
+    console.log(url);
+});
