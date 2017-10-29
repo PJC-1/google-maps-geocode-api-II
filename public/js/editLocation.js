85283c2f8c8c108c1534409e2f7ffcd9f919299a
PJC-1 <jumpzine@gmail.com>
2017-10-28 18:05:50 -0700

    making a request on page load of the individual location for edit

--- public/js/editLocation.js
+++ public/js/editLocation.js
@@ -7,6 +7,24 @@ $(document).ready(function(){
     var url = '/api/locations/' + id;
 
     console.log(url);
+
+    // make a request to fetch the location
+    $.ajax({
+      method : "GET",
+      url : url,
+      success : onSuccess,
+      error : onError
+    });
+
     // use something like document.getElementById('location-input').value = ${formattedAddress};
     // so that the form is populated with the location's address
+    // document.getElementById('location-input').value =
 });
+
+function onSuccess(result){
+  console.log(result);
+}
+
+function onError(err){
+  console.log(err);
+}
